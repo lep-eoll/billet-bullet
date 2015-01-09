0edb8133c8b597cc088a3c5d53fc7e2994e4645e
Ivar Vasara <ivar@oobik.com>
2015-01-09 04:07:29 -0800

    adding start of lep theme

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150109112105) do
+ActiveRecord::Schema.define(version: 20150109120614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
