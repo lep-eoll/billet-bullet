45bb8ab1415ec67e9ab328a62900fce2d3f30b90
Ivar Vasara <ivar@oobik.com>
2015-02-15 22:31:20 -0800

    testing emails configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,13 @@ Rails.application.configure do
     }
   end
 
+  ActionMailer::Base.smtp_settings = {
+      :port =>           '587',
+      :address =>        'smtp.mandrillapp.com',
+      :user_name =>      ENV['MANDRILL_USERNAME'],
+      :password =>       ENV['MANDRILL_APIKEY'],
+      :domain =>         'lep2015.com',
+      :authentication => :plain
+  }
+  ActionMailer::Base.delivery_method = :smtp
 end
