2fdd394ac1c64a99af0b591a9756f20328050b5e
Ivar Vasara <ivar@oobik.com>
2015-02-15 22:46:38 -0800

    adding http basic auth until ready

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :authenticate_beta_environments
+
+  def authenticate_beta_environments
+    if ENV['HTTP_BASIC_PASSWORD'].present?
+      authenticate_or_request_with_http_basic(realm = "LEP2015 Ecommerce Site (BETA)") do |username, password|
+        username == ENV['HTTP_BASIC_PASSWORD'] && password == ENV['HTTP_BASIC_PASSWORD']
+      end
+    end
+  end
+
 end
