9cf7fc64d946ca9fb2afb8637782b9c6d7b4991a
Ivar Vasara <ivar@oobik.com>
2015-02-22 04:11:48 -0800

    hiding navigation during checkout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,10 @@ module ApplicationHelper
   def home_link_from_nav
     if ticket_homepage?
       link_to '&larr; LEP2015 Home'.html_safe, 'http://lep2015.com'
+    elsif checkout_page?
+      '&nbsp;'.html_safe
     else
+
       link_to '&larr; Pilleti Indeks / Back to Tickets'.html_safe, spree.root_path
     end
   end
@@ -12,5 +15,9 @@ module ApplicationHelper
 
   def ticket_homepage?
     request.env['PATH_INFO'] == '/'
+  end
+
+  def checkout_page?
+    ['/cart', '/checkout'].any? {|path| request.env['PATH_INFO'].downcase.include? path}
   end
 end
