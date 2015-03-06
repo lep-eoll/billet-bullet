8dc4ed11aa6dc292fc80b3d670e5ad4591a5860a
Ivar Vasara <ivar@oobik.com>
2015-03-05 21:54:26 -0800

    remove beta password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :authenticate_beta_environments
+  #before_filter :authenticate_beta_environments
 
-  def authenticate_beta_environments
-    if ENV['HTTP_BASIC_PASSWORD'].present?
-      authenticate_or_request_with_http_basic(realm = "LEP2015 Ecommerce Site (BETA)") do |username, password|
-        username == ENV['HTTP_BASIC_PASSWORD'] && password == ENV['HTTP_BASIC_PASSWORD']
-      end
-    end
-  end
+  #def authenticate_beta_environments
+    #if ENV['HTTP_BASIC_PASSWORD'].present?
+      #authenticate_or_request_with_http_basic(realm = "LEP2015 Ecommerce Site (BETA)") do |username, password|
+        #username == ENV['HTTP_BASIC_PASSWORD'] && password == ENV['HTTP_BASIC_PASSWORD']
+      #end
+    #end
+  #end
 
 end
