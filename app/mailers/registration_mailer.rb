5808b479af77baef7f7f9224d18f845724fe1b14
Ivar Vasara <ivar@oobik.com>
2015-06-01 00:55:30 -0700

    email blast for registration collection

---
+++ app/mailers/registration_mailer.rb
@@ -0,0 +1,13 @@
+class RegistrationMailer < ActionMailer::Base
+  default from: "info@lep2015.com"
+
+  def data_collection_email(order, num_greetings, num_ball_tix, num_regs, registration_url)
+    @order = order
+    @num_greets = num_greetings
+    @num_ball_tix = num_ball_tix
+    @num_registrations = num_regs
+    @reg_url = registration_url
+
+    mail(to: 'ivar@oobik.com', cc: 'liligo@shaw.ca', subject: 'LEP2015 Registration Info Needed for Your Event Tickets!')
+  end
+end
